da732737bb287c13abd8d95c74a4be300a03ac12
minseominseo <chj2171@gmail.com>
2021-02-04 20:36:12 +0900

    어제 프로그래머스 카카오 아이디해결

--- src/proGrammers/prgrammer.java
+++ src/proGrammers/prgrammer.java
@@ -1,26 +1,30 @@
 package proGrammers;
 
+import java.util.Scanner;
+
 public class prgrammer {
     public static void main(String args[]){
-
+        Scanner sc = new Scanner(System.in);
+        String temp3 = sc.next();
+        System.out.println(solution(temp3));
     }
-}
-class Solution {
-    public String solution(String new_id) {
+
+    public static String solution(String new_id) {
         String answer = new_id.toLowerCase();
-        String pattern = "[0-9a-z-_\\.]";
-        String pattern2 = "\\.{2,}";
+        String pattern = "[^0-9a-z-_\\.]";
+        String pattern2 = "[\\.]{2,}";
         answer = answer.replaceAll(pattern,"");
         answer = answer.replaceAll(pattern2,".");
         if(answer.charAt(0)=='.') answer = answer.substring(1);
-        if(answer.equals(""))answer.concat("a");
-        if(answer.length()>16)answer = answer.substring(0,15);
+        if(answer.equals(""))answer=answer.concat("a");
+        if(answer.length()>15)answer = answer.substring(0,15);
         int len = answer.length();
-        if(answer.charAt(len)=='.')answer = answer.substring(0,len-1);
+//        System.out.println(answer);
+        if(answer.charAt(len-1)=='.')answer = answer.substring(0,len-1);
         if(answer.length()<3){
             String temp = Character.toString(answer.charAt(answer.length()-1));
-            while(answer.length()!=3){
-                answer.concat(temp);
+            for(int i=answer.length(); i<3; i++){
+               answer= answer.concat(temp);
             }
         }
         return answer;
