e8ad6a3a01ce1ca56e89d5ced5a5ea33b1bb134a
minseominseo <chj2171@gmail.com>
2021-02-15 23:02:14 +0900

    이진탐색 열심히 해봐야겠다....

---
+++ src/Beak/numcard_10816.java
@@ -0,0 +1,12 @@
+package Beak;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class numcard_10816 {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
