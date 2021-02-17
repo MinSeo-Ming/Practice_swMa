19edb0685e64b1d8eee3d2f0dba835f63f62b0fe
minseominseo <chj2171@gmail.com>
2021-02-17 22:07:13 +0900

    효율성 문제 해결...!!

--- src/proGrammers/hot.java
+++ src/proGrammers/hot.java
@@ -4,41 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class hot {
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
         System.out.println(solution(new int[]{10, 10, 10, 10, 10}, 1));
     }
     public static int solution(int[] scoville, int K) {
         int answer = 0;
-        Arrays.sort(scoville);
-        ArrayList<Integer> temp = new ArrayList<Integer>(Arrays.stream(scoville).boxed().collect(Collectors.toList()));
+        PriorityQueue<Integer> q = new PriorityQueue<>();
         int t;
-        if(temp.get(0)>=K) return 0;
+        for(int i: scoville) q.add(i);
         while (true) {
-            t = temp.get(0) + temp.get(1) * 2;
+            if(q.size()<=1 || q.peek()>=K) break;
+            t = q.poll() + q.poll() * 2;
             answer++;
-//            System.out.println(t+" "+answer);
-            temp.remove(1);
-            temp.remove(0);
-            temp.add(0, t);
-            if (t >= K) {
-                if (temp.size() == 1)  break;
-                else if (temp.get(1) >= K) break;
-            }
-            else if(temp.size()==1) {
-                answer = -1;
-                break;
-            }
-            Collections.sort(temp);
+            q.add(t);
         }
+        if(q.size()<=1&&q.peek()<K)
+            answer=-1;
         return answer;
     }
 }
