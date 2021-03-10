9c8f32718b0e387bd3e0fd45fa23237a300b46f8
minseominseo <chj2171@gmail.com>
2021-03-10 21:51:55 +0900

    아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 복붙 조심해서 하자...

--- src/Beak/tree_order.java
+++ src/Beak/tree_order.java
@@ -1,93 +1,72 @@
 package Beak;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Node{
-    String d;
-    Node left=null;
-    Node right=null;
-
-    public Node(String d){
-        this.d = d;
-    }
-
-    public String getD() {
-        return d;
-    }
-}
+import java.io.*;
+import java.util.*;
 
 public class tree_order {
-    public Node root;
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int node = Integer.parseInt(br.readLine());
 
-    public void create(String d, String left, String right){
-        if(root ==null){
-            root = new Node(d);
-            if(!left.equals(".")) {
-                root.left = new Node(left);
-            }
-            else if(!right.equals(".")){
-                root.right= new Node(right);
-            }
-        }else {
-            search(root,d,left,right);
+        tree t = new tree();
+        char[] s;
+        for(int i=0; i<node; i++){
+            s = br.readLine().replaceAll(" ","").toCharArray();
+            t.create(s[0],s[1],s[2]);
         }
-
+        t.preorder(t.root);
+        System.out.println();
+        t.inorder(t.root);
+        System.out.println();
+        t.postorder(t.root);
+        br.close();
     }
-    public void search(Node node, String d, String left, String right){
-        if(node==null) {
-            return;
-        }
-        else if(node.getD().equals(d)){
-            System.out.println("Node 찾기 "+d+" "+left+" "+right);
-            if(!left.equals(".")){
-                root.left= new Node(left);
-            }
-            else if(!right.equals(".")){
-                root.right= new Node(right);
-            }
+}
+class Node{
+    char d;
+    Node left,right;
+    public Node(char d){
+        this.d = d;
+    }
+}
+class tree{
+    Node root;
+    public void create(char d, char left, char right){
+        if(root ==null){
+            if(d !='.') root = new Node(d);
+            if(left!='.') root.left = new Node(left);
+            if(right!='.') root.right= new Node(right);
+        }else search(root,d,left,right);
+    }
+    public void search(Node root, char d, char left, char right){
+        if(root==null) return;
+         else if(root.d==d){
+            if(left!='.') root.left= new Node(left);
+            if(right!='.')root.right= new Node(right);
         }else {
-            search(node.left,d,left,right);
-            search(node.right,d,left,right);
+            search(root.left,d,left,right);
+            search(root.right,d,left,right);
         }
     }
     public void preorder(Node n){
         if(n !=null){
-            System.out.print(n.d+" ");
+            System.out.print(n.d);
             if(n.left!=null) preorder(n.left);
             if(n.right!=null) preorder(n.right);
         }
     }
     public void inorder(Node n){
         if(n !=null){
-            if(n.left!=null) preorder(n.left);
-            System.out.print(n.d+" ");
-            if(n.right!=null) preorder(n.right);
+            if(n.left!=null) inorder(n.left);
+            System.out.print(n.d);
+            if(n.right!=null) inorder(n.right);
         }
     }
     public void postorder(Node n){
         if(n !=null){
-            if(n.left!=null) preorder(n.left);
-            if(n.right!=null) preorder(n.right);
-            System.out.print(n.d+" ");
+            if(n.left!=null) postorder(n.left);
+            if(n.right!=null) postorder(n.right);
+            System.out.print(n.d);
         }
-    }
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int node = Integer.parseInt(br.readLine());
-        String[] s;
-
-        tree_order t = new tree_order();
-        for(int i=0; i<node; i++){
-            s = br.readLine().split(" ");
-            t.create(s[0],s[1],s[2]);
-        }
-       t.preorder(t.root);
-        System.out.println();
-       t.inorder(t.root);
-        System.out.println();
-       t.postorder(t.root);
     }
 }
